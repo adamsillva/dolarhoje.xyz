e61a9cd3a13f16208828a53450c58120a1ed21f4
Àdams Sillva <113443262+adamsillva@users.noreply.github.com>
2025-03-27 23:13:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Dólar Hoje',
+  description: 'Dólar Hoje: Cotação comercial do dólar americano',
+  generator: '',
 }
 
 export default function RootLayout({
